a75adb447d34037c8cd3c0b6e6fa3199ecd95d14
Eecodev <mnasser.bbk@gmail.com>
2023-11-13 21:38:15 +0100

    single card component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-
+    <div class="card bg-transparent border-0 ">
+        <img :src="image" class="card-img-top" :alt="title">
+        <div class="card-body text-center">
+            <h5 class="card-title">{{ title }}</h5>
+            <h5 class="card-title">{{ subtitle }}</h5>
+            <h5 class="card-title">{{ text }}</h5>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'CardComponent',
+        props: [
+            'image',
+            'title',
+            'subtitle',
+            'text'
+        ]
     }
 </script>
 

eb887734746a85319b3f443713285ecf91e854f4
Eecodev <mnasser.bbk@gmail.com>
2023-11-13 22:33:38 +0100

    section card added to main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,28 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <div class="row gy-4">
+            <div class="col-12 col-md-4 col-lg-3" v-for="(card, index) in store.cardList" :key="card.id">
+                <CardComponent :image="card.card_images" :title="card.name" :subtitle="card.archetype" :text="card.desc"/>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-    export default {
-        
-    }
+import {store} from '../data/store.js';
+import CardComponent from './CardComponent.vue';
+
+export default {
+    name: 'MainComponent',
+    component: {
+        CardComponent
+    },
+    data() {
+        return {
+            store
+        };
+    },
+}
 </script>
 
 <style lang="scss" scoped>
